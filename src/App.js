c64787467f54f9b3488ea4277f58d587236e06cf
Aakash Goel <goelaakash79@gmail.com>
2019-07-28 14:39:25 +0530

    folder structure + basic code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Route,
+  Switch,
+  Redirect,
+  BrowserRouter as Router
+} from "react-router-dom";
+// import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/stylesheets/style.css";
+import Navbar from "./components/Navbar/Navbar";
+// import HomePage from "./components/HomePage/HomePage";
+// import Login from "./components/Authentication/Login";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Navbar />
+        {/* <Route exact path="/" component={HomePage} />
+        <Route exact path="/login" component={Login} />         */}
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+import {Link} from "react-router-dom";
+import logo from "../../assets/images/logo (2).svg"
+class Navbar extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <div className="container">
+            <img
+              src={logo}
+              alt="logo"
+              width="48px"
+              height="54px"
+              style={{marginRight: "50px"}}
+            />
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <i className="fa fa-bars" />
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto nav">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    About us
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Way to Help
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Projects
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Team
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Contact us
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    href="#"
+                    className="nav-link"
+                    style={{color: "rgb(213,8,8)!important"}}
+                  >
+                    Donate now
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    href="#"
+                    className="nav-link"
+                    style={{color: "rgb(213,8,8)!important"}}
+                  >
+                    Be a Volunteer
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
